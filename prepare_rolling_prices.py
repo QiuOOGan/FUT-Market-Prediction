00a4de925da80a80bab2a4cde95f62f02c48250e
QiuOOGan <ganqiu@ucdavis.edu>
2021-03-16 16:21:03 -0500

    rolling prices prepared

---
+++ prepare_rolling_prices.py
@@ -0,0 +1,45 @@
+import json
+import os
+import glob
+import pandas as pd
+import numpy as np
+from sklearn import preprocessing
+
+def prepare_rolling_prices(df):
+	f = open('./utils/fodders.json')
+	fodder_dict = json.load(f)
+	global min_price
+	global max_price
+	global mean_price
+	min_price = min(pd.array(df["price"]))
+	max_price = max(pd.array(df["price"]))
+	mean_price = np.mean(pd.array(df["price"]))
+
+	rolling = []
+
+
+	for key, value in fodder_dict.items():
+		this_df = df.loc[df['ID'] == int(value)]["price"]
+		this_df = this_df.astype(int)
+		
+
+		# min max normalization
+		this_df = min_max_normalize(this_df)
+		this_np_array = np.array(this_df)
+		# Create Rolling Prices
+
+		for i in range(30, len(this_np_array)):
+			this_date = []
+			for j in range(i-30, i+1):
+				this_date.append(this_np_array[j])
+			rolling.append(this_date)
+
+	return pd.DataFrame(rolling)
+
+
+def min_max_normalize(df):
+	return (df - mean_price) / (max_price - min_price)
+
+if __name__ == '__main__':
+	training = pd.read_csv("./training_files/training.csv")
+	prepare_rolling_prices(training)
