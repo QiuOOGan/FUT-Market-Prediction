fa6a855f4523b6441fe7ccece0f18b18fdc54e4c
QiuOOGan <ganqiu@ucdavis.edu>
2021-03-17 00:06:16 -0500

    testing

--- lstm_train.py
+++ lstm_train.py
@@ -19,22 +19,22 @@ def trainLSTM():
 	x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
 
 	testing = pd.read_csv("./training_files/testing.csv")
-	x_test, y_test = prepare_rolling_prices(training)
+	x_test, y_test = prepare_rolling_prices(testing)
 	x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
 
 	print("Data Preparation Completed")
-	epochs = 100
+	epochs = 500
 	batch_size = 10
 
 	# create Model
 	model = Sequential()
-	model.add(LSTM(50, return_sequences=False, stateful=True, batch_input_shape = (batch_size,x_train.shape[1], 1)))  # 2 time step, 1 feature
-	# model.add(LSTM(50, return_sequences = False))
-	model.add(Dense(3))
+	model.add(LSTM(64, return_sequences=True, stateful=True, batch_input_shape = (batch_size,x_train.shape[1], 1)))  # 2 time step, 1 feature
+	model.add(LSTM(64, return_sequences = True))
+	model.add(LSTM(64))
+	model.add(Dense(20))
 	model.add(Dense(1)) # 1 output: Price
 
 	# Train
-
 	train_scores = []
 	test_scores = []
 	train_loss = LambdaCallback(on_epoch_end=lambda batch, logs: train_scores.append(logs['loss']))
@@ -43,7 +43,7 @@ def trainLSTM():
 	test_loss = LambdaCallback(on_epoch_end=lambda batch, logs: test_scores.append(model.evaluate(x_test, y_test, batch_size=batch_size)[0]))
 	model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, callbacks=[train_loss, test_loss, earlystopper])
 	result = model.evaluate(x_test, y_test, batch_size=batch_size)[1]
-	model.save('./models/my_model_2.h5')
+	model.save('./models/my_model_4.h5')
 	print("Training Completed")
 
 	plt.figure()
